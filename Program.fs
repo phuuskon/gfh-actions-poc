7d2827f4d28b860f435f9761f4a840c4bb3240d4
Huuskonen Pekka <pekka.huuskonen@elisa.fi>
2021-02-24 21:30:59 +0200

    farmer stuff

---
+++ Program.fs
@@ -0,0 +1,26 @@
+open Farmer
+open Farmer.Builders
+open System
+
+// Create ARM resources here e.g. webApp { } or storageAccount { } etc.
+// See https://compositionalit.github.io/farmer/api-overview/resources/ for more details.
+let stg = storageAccount {
+    name "stelisaghactionspoc"
+    sku Storage.Sku.Standard_LRS
+}
+
+// Add resources to the ARM deployment using the add_resource keyword.
+// See https://compositionalit.github.io/farmer/api-overview/resources/arm/ for more details.
+let deployment = arm {
+    location Location.WestEurope
+    add_resource stg
+}
+
+printf "Generating ARM template..."
+deployment |> Writer.quickWrite "output"
+printfn "all done! Template written to output.json"
+
+// Alternatively, deploy your resource group directly to Azure here.
+// deployment
+// |> Deploy.execute "farmer-resource-group" Deploy.NoParameters
+// |> printfn "%A"
