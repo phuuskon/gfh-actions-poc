31cac02b4c9aa8f486b07b1cd4b826707d07fc75
Huuskonen Pekka <pekka.huuskonen@elisa.fi>
2021-02-25 07:59:38 +0200

    changed login and deployment

--- Program.fs
+++ Program.fs
@@ -5,10 +5,8 @@ open System
 [<EntryPoint>]
 let main argv =
 
-    let azAppId = argv.[0]
-    let azSecret = argv.[1]
-    let azTenantId = argv.[2]
-
+    let rgName = argv.[0]
+    
     // Create ARM resources here e.g. webApp { } or storageAccount { } etc.
     // See https://compositionalit.github.io/farmer/api-overview/resources/ for more details.
     let stg = storageAccount {
@@ -24,16 +22,8 @@ let main argv =
     }
 
 
-    //printf "azAppId: %s" azAppId
-    //deployment |> Writer.quickWrite "output"
-    //printfn "all done! Template written to output.json"
-
-    // Alternatively, deploy your resource group directly to Azure here.
-    Deploy.authenticate azAppId azSecret azTenantId
-    |> ignore
-
     deployment
-    |> Deploy.execute "rg-ghactions-poc" Deploy.NoParameters
+    |> Deploy.execute rgName Deploy.NoParameters
     |> printfn "%A"
 
     0
